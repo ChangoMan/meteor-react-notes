6e42f477ab900e567d41525f86cb543982c01747
Hunter Chang <hunter@madwiremedia.com>
2017-08-17 15:08:11 -0600

    initial commit

---
+++ imports/ui/NoteListHeader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+
+export const NoteListHeader = (props) => {
+    return (
+        <div>
+            Note List Header
+            <button onClick={() => {
+                props.meteorCall('notes.insert');
+            }}>New Note</button>
+        </div>
+    )
+};
+
+NoteListHeader.propTypes = {
+    meteorCall: React.PropTypes.func.isRequired
+}
+
+export default createContainer(() => {
+    return {
+        meteorCall: Meteor.call
+    }
+}, NoteListHeader);
